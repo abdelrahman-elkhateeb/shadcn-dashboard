e3ebc1e57ec98e127976f66726d80b6b4889b10f
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-01-08 23:17:25 +0200

    Tanstack Table Action Button with ShadCN

--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -262,6 +262,7 @@ const getData = async (): Promise<Payment[]> => {
 export default async function page() {
   const data = await getData();
   return (
+    
     <div className="">
       <div className="mb-8 px-4 py-2 bg-secondary rounded-md">
         <h1 className="font-semibold">All Payments</h1>
