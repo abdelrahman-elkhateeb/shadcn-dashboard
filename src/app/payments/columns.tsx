38ba7e4e4f7a1be4ee276be059433ddc12f9a377
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-01-08 23:12:25 +0200

    ShadCN Data Table with Tanstack React Table

---
+++ src/app/payments/columns.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { cn } from "@/lib/utils";
+import { ColumnDef } from "@tanstack/react-table"
+
+export type Payment = {
+  id: string;
+  amount: number;
+  username: string;
+  email: string;
+  status: "pending" | "processing" | "success" | "failed";
+}
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "username",
+    header: "User",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "status",
+    cell: ({ row }) => {
+      const status = row.getValue("status")
+      return (<div className={cn(`p-1 rounded-md w-max text-xs`,
+        status === "pending" && "bg-yellow-500/40",
+        status === "failed" && "bg-red-500/40",
+        status === "success" && "bg-green-500/40"
+      )}>
+        {status as string}
+      </div>)
+    }
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"))
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount)
+
+      return <div className="text-right font-medium">{formatted}</div>
+    },
+  }
+]
